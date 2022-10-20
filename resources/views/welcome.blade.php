b44619bf668cce0e00a1afeed713f27fd0ba41e0
Hafsa Omar <66945040+hafsa-omar@users.noreply.github.com>
2022-10-20 20:37:15 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,14 @@
                         </g>
                     </svg>
                 </div>
-
+                
+  <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+WELCOME TO MY WEBSITE                                </div>
+                            </div>
+                        </div>
+                        
+                        
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
